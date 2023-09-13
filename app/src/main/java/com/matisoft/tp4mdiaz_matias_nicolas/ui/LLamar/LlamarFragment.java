823c11b0f6eaa6adc5832f2d433f38a8a657f724
matydiaz434233 <102767133+matydiaz434233@users.noreply.github.com>
2023-09-13 08:54:58 -0300

    Update LlamarFragment.java

--- app/src/main/java/com/matisoft/tp4mdiaz_matias_nicolas/ui/LLamar/LlamarFragment.java
+++ app/src/main/java/com/matisoft/tp4mdiaz_matias_nicolas/ui/LLamar/LlamarFragment.java
@@ -1,5 +1,10 @@
 package com.matisoft.tp4mdiaz_matias_nicolas.ui.LLamar;
 
+import static androidx.core.content.PermissionChecker.checkSelfPermission;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +12,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -18,26 +26,39 @@ public class LlamarFragment extends Fragment {
     private FragmentLlamarBinding binding;
     private LlamarViewModel vm;
 
+    private static final int REQUEST_CALL_PHONE_PERMISSION = 1;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        LlamarViewModel llamarViewModel =
-                new ViewModelProvider(this).get(LlamarViewModel.class);
-
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        vm = new ViewModelProvider(this).get(LlamarViewModel.class);
         binding = FragmentLlamarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-       // final TextView textView = binding.idNumero;
-        //llamarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         binding.btLlamar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(vm.telefono(binding.idNumero.toString()));
+                // Antes de realizar la llamada, verifica si tienes el permiso CALL_PHONE
+                if (chequeaPermiso(Manifest.permission.CALL_PHONE, REQUEST_CALL_PHONE_PERMISSION)) {
+                    // Tienes permiso, puedes realizar la acción aquí
+                    startActivity(vm.telefono(binding.idNumero.getText().toString()));
+                }
             }
         });
 
         return root;
     }
+
+    // Método para verificar si tienes permiso
+    private boolean chequeaPermiso(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+            // Si el permiso no ha sido concedido, solicítalo
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{permission}, requestCode);
+            return false;
+        }
+        return true;
+    }
+
 
     @Override
     public void onDestroyView() {
